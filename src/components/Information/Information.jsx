b69670718e56f0177995fae2e3d2eed43cbb7bb0
VincentLee1208 <vinlee1208@gmail.com>
2022-09-01 13:06:00 -0700

    Finished most of date and cabin/traveler info

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import { getAirports } from './../../api';
 import './Information.css';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 import SwapHorizIcon from '@material-ui/icons/SwapHoriz';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
 
 const Information = () => {
     const [airportsOrigin, setOriginAirports] = useState([]);
@@ -10,6 +16,16 @@ const Information = () => {
     const [timer, setTimer] = useState(null);
     const [origin, setOrigin] = useState('');
     const [destination, setDestination] = useState('');
+
+    const [startDate, setStartDate] = useState(new Date());
+    const [returnDate, setReturnDate] = useState(new Date());
+
+    const [seeDrop, setSeeDrop] = useState(true);
+
+    const [flightClass, setFlightClass] = useState('economy');
+    const [numAdults, setNumAdults] = useState(1);
+    const [numChild, setNumChild] = useState(0);
+
 
 
     const handleOriginChange = (e) => {
@@ -63,25 +79,160 @@ const Information = () => {
         setDestination(start)
     }
 
+    //Can maybe turn inline instead after to test if works
+    const MinusAdultPassenger = () => {
+        var adults = numAdults
+        adults--
+        setNumAdults(adults)
+    }
+
+    const AddAdultPassenger = () => {
+        var adults = numAdults
+        adults++
+        setNumAdults(adults)
+    }
+
+    const MinusChildPassenger = () => {
+        var children = numChild
+        children--
+        setNumChild(children)
+    }
+
+    const AddChildPassenger = () => {
+        var children = numChild
+        children++
+        setNumChild(children)
+    }
+
     return (
         <form>
-            <h5>From: </h5>
-            <input type="text" id="origin" name="origin" onChange={handleOriginChange} value={origin} />
-            {airportsOrigin.length > 0 ? 
-            airportsOrigin.map((airport) => {
-                return <button value={airport.AirportCode} onClick={setOriginAirport}>{airport.AirportName} {airport.AirportCode}</button>
-            }) : <div></div>    
-            }
-            <SwapHorizIcon id="icon" onClick={swapLocations} />
+            <table>
+                <tr>
+                    <td>
+                        <h5>From: </h5>
+                    </td>
+                    <td>
 
-            <h5>To: </h5>
-            <input type="text" id="destination" name="destination" onChange={handleDestChange} value={destination}/>
-            {airportsDest.length > 0 ?
-            airportsDest.map((airport) => {
-                return <button value={airport.AirportCode} onClick={setDestAirport}>{airport.AirportName} {airport.AirportCode}</button>
-            }) : <div></div>
-            }
+                    </td>
+                    <td>
+                        <h5>To: </h5>
+                    </td>
+                    <td>
+                        <h5>Depart: </h5>
+                    </td>
+                    <td>
+                        <h5>Return: </h5>
+                    </td>
+                    <td>
+                        <h5>Cabin Class & Travelers</h5>
+                    </td>
+                </tr>
+                <tr>
+                    <td id='input'>
+                        <input type="text" id="origin" name="origin" onChange={handleOriginChange} value={origin} />
+                    </td>
+                    <td>
+                        <SwapHorizIcon id="icon" onClick={swapLocations} />
+                    </td>
+                    <td id='input'>
+                        <input type="text" id="destination" name="destination" onChange={handleDestChange} value={destination}/>
+                    </td>
+                    <td id='input'>
+                        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}/>
+                    </td>
+                    <td id='input'>
+                        <DatePicker selected={returnDate} onChange={(date) => setReturnDate(date)}/>
+                    </td>
+                    <td id='input'>
+                        <button type="button" id="dropbutton" onClick={() => setSeeDrop(!seeDrop)}>{flightClass}</button> 
+                    </td>
+                </tr>
+                <tr>
+                    <td id='input'>
+                        {airportsOrigin.length > 0 ? 
+                        airportsOrigin.map((airport) => {
+                            return <button value={airport.AirportCode} onClick={setOriginAirport}>{airport.AirportName} {airport.AirportCode}</button>
+                        }) : <div></div>    
+                        }
+                    </td>
+                    <td></td>
+                    <td id='input'>
+                        {airportsDest.length > 0 ?
+                        airportsDest.map((airport) => {
+                            return <button value={airport.AirportCode} onClick={setDestAirport}>{airport.AirportName} {airport.AirportCode}</button>
+                        }) : <div></div>
+                        }
+                    </td>
+                    <td></td>
+                    <td colspan="2">
+                        {seeDrop ? <></> : 
+                        <div id="drop">
+                            <div id='dropdown'>
+                                <h4>Cabin class</h4>
+                                <select onChange={(e) => setFlightClass(e.target.value)}>
+                                    <option value='economy'>Economy</option>
+                                    <option value='premiumeconomy'>Premium Economy</option>
+                                    <option value='business'>Business Class</option>
+                                    <option value='first-class'>First-Class</option>
+                                </select>
+
+                                <h4>Adults(16+)</h4>
+                                <table>
+                                    <tr>
+                                        <td id="passengers">
+                                            {numAdults === 1 ? 
+                                            <RemoveCircleIcon id="circleIcon"/> : 
+                                            <RemoveCircleOutlineIcon id="circleIcon" onClick={MinusAdultPassenger}/>
+                                            }
+                                        </td>
+                                        <td>
+                                            <h2>{numAdults}</h2>
+                                        </td>
+                                        <td id="passengers">
+                                            {numAdults === 8 ?
+                                            <AddCircleIcon id="circleIcon"/>:
+                                            <AddCircleOutlineIcon id="circleIcon" onClick={AddAdultPassenger}/>
+                                            }
+                                        </td>
+                                    </tr>
+                                </table>
+
+                                <h4>Children(0-15)</h4>
+                                <table>
+                                    <tr>
+                                        <td id="passengers">
+                                            {numChild === 0 ? 
+                                            <RemoveCircleIcon id="circleIcon"/> : 
+                                            <RemoveCircleOutlineIcon id="circleIcon" onClick={MinusChildPassenger}/>
+                                            }
+                                        </td>
+                                        <td>
+                                            <h2>{numChild}</h2>
+                                        </td>
+                                        <td id="passengers">
+                                            {numChild === 8 ?
+                                            <AddCircleIcon id="circleIcon"/>:
+                                            <AddCircleOutlineIcon id="circleIcon" onClick={AddChildPassenger}/>
+                                            }
+                                        </td>
+                                    </tr>
+                                </table>
+                                   
+                                
+
+
+                                
+
+                            </div>
+                        </div>
+                        }
+                    </td>
+                </tr>
+            </table>
         </form>
+        
+
+
     );
 }
 
